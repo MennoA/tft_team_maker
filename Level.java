b8550b216f0cbddf7a4b392d4ff4af32b0c571da
julien.fah <julien.fah@etu.hesge.ch>
2022-10-09 16:32:21 +0200

    add champion chance array

---
+++ Level.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+public class Level {
+   public static List<List<Integer>> level_mapping = List.of(
+      List.of(100, 0, 0, 0, 0), 
+      List.of(100, 0, 0, 0, 0), 
+      List.of(75, 25, 0, 0, 0), 
+      List.of(55, 30, 15, 0, 0), 
+      List.of(45, 33, 20, 2, 0), 
+      List.of(25, 40, 30, 5, 0), 
+      List.of(19, 30, 35, 15, 1), 
+      List.of(16, 20, 35, 25, 4), 
+      List.of(9, 15, 30, 30, 16), 
+      List.of(5, 10, 20, 40, 25), 
+      List.of(1, 2, 12, 50, 35));
+}
