dc919053d0c657022389e4bb69881ebc928960ff
julien.fah <julien.fah@etu.hesge.ch>
2022-10-09 16:12:27 +0200

    update unit size for unit like dragon

--- Trait.java
+++ Trait.java
@@ -5,11 +5,9 @@ public class Trait {
    String description;
    // to be updated in the future with the bonus at each step
    List<Integer> upgrade_counts;
-   Integer unit_size;
 
    public Trait(String name, List<Integer> upgrade_counts){
       this.name = name;
-      this.unit_size = 1;
       this.upgrade_counts = upgrade_counts;
    }
 }
--- Unit.java
+++ Unit.java
@@ -4,6 +4,7 @@ class Unit {
     String name;
     String tier;
     int cost;
+    int unit_size = 1;
     Map<Trait, Integer> traits;
 
     public Unit(String name, String tier, int cost, Map<Trait, Integer> traits){
@@ -11,10 +12,15 @@ class Unit {
         this.tier = tier;
         this.cost = cost;
         this.traits = traits;
+        for (Trait t : traits.keySet()) {
+            if(t.name == "dragon"){
+                this.unit_size = 2;
+            }
+        }
     }
 
     public void print_info(){
-        System.out.printf("%s\ncost: %d\nrarety: %s\ntraits:\n", name, cost, tier);
+        System.out.printf("%s\ncost: %d\nrarety: %s\nunit size : %d\ntraits:\n", name, cost, tier, unit_size);
             for (Trait trait : traits.keySet()) {
                 System.out.printf("\t%s\n", trait.name);
             }
